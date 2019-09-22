aad660fed36ae133158668c1181ee8931d4ae90c
mzmt <mrcrius1355@gmail.com>
2019-09-22 15:50:39 +0900

    ファイルがない場合に初期化する

--- db_by_csv.rb
+++ db_by_csv.rb
@@ -1,12 +1,23 @@
 require 'csv'
 
 class Database
+  def initialize
+    unless File.exist?('db.csv')
+      CSV.open('db.csv', 'w') do |column|
+        column << ['A', 'B', 'C']
+      end
+    end
+  end
+
   def add_column(value)
     csv = CSV.read('db.csv')
     CSV.open('db.csv', 'w') do |column|
       csv[0] << value
       column << csv[0]
     end
+  end
+
+  def add_row(value)
   end
 end
 
